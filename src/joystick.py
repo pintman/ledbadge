09595ba001d1582448333db697bb380cc8b3e743
Marco Bakera <bakera@tbs1.de>
2018-10-23 18:25:53 +0200

    added joystick class.

---
+++ src/joystick.py
@@ -0,0 +1,45 @@
+from machine import Pin
+
+
+class Joystick:
+    def __init__(self, w, a, s, d, btn):
+        'create joystick with given pins for directions and button.'
+
+        self.directions = {'w': w, 'a': a, 's': s, 'd': d}
+        for pin in self.directions.values():
+            assert isinstance(pin, Pin)
+
+        assert isinstance(btn, Pin)
+        self.btn = btn
+
+    def btn_pressed(self):
+        'Return whether button is pressed.'
+        return self.btn.value() == 1
+
+    def direction(self):
+        'get the current direction. One of w, a, s, d or None.'
+
+        for wasd in self.directions:
+            if self.directions[wasd].value():
+                return wasd
+
+        # no pressed direction found
+        return None
+
+
+# GPIO pin numbers
+D0 = 16
+D1 = 5
+D2 = 4
+D3 = 0
+D4 = 2
+# D6 = 12
+# D8 = 15
+
+joy = Joystick(
+    w=Pin(D1, mode=Pin.IN, pull=Pin.PULL_UP),
+    a=Pin(D2, mode=Pin.IN, pull=Pin.PULL_UP),
+    s=Pin(D3, mode=Pin.IN, pull=Pin.PULL_UP),
+    d=Pin(D4, mode=Pin.IN, pull=Pin.PULL_UP),
+    btn=Pin(D0, mode=Pin.IN)
+)
