736adae164b0552558ef520668a16fadb479456f
Marco Bakera <bakera@tbs1.de>
2018-10-23 17:43:40 +0200

    added file for button test.

---
+++ src/btntest.py
@@ -0,0 +1,29 @@
+from machine import Pin
+import time
+
+# GPIO pin numbers
+D0 = 16
+D1 = 5
+D2 = 4
+D3 = 0
+D4 = 2
+
+pins = [
+    Pin(D0, mode=Pin.IN, pull=Pin.PULL_UP),
+    Pin(D1, mode=Pin.IN, pull=Pin.PULL_UP),
+    Pin(D2, mode=Pin.IN, pull=Pin.PULL_UP),
+    Pin(D3, mode=Pin.IN, pull=Pin.PULL_UP),
+    Pin(D4, mode=Pin.IN, pull=Pin.PULL_UP),
+]
+
+
+def main():
+    print("Running pin test")
+    while True:
+        s = ''
+        for p in pins:
+            s += str(p.value()) + ' '
+
+        print(s)
+
+    time.sleep(1)
