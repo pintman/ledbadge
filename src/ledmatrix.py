804bc485251b9d0fe9294851f6045a3bef2ea044
Marco Bakera <marco@bakera.de>
2018-10-24 06:46:00 +0200

    doc.

--- src/ledmatrix.py
+++ src/ledmatrix.py
@@ -10,6 +10,7 @@ class LedMatrix:
         self._buffer = [0] * 8
 
     def px(self, x, y, val):
+        'Set pixel at x,y to be on or off. Needs invocation of show afterwards.'
         assert 0 <= x <= self.width
         assert 0 <= y <= self.height
 
@@ -19,6 +20,7 @@ class LedMatrix:
             self._buffer[y] = self._buffer[y] & ~(2**x)
 
     def show(self):
+        'Show the current buffer on the display.'
         self.tm.write(self._buffer)
 
     def scroll(self, fill=0):
@@ -26,5 +28,5 @@ class LedMatrix:
         self._buffer = [fill] + self._buffer[:-1]
 
     def clear(self):
-        'Clear the matrix screen buffer'
+        'Clear the matrix screen buffer.'
         self._buffer = [0] * 8
