5ddd6a89372884c00d094fdb56cb57aac81ced5d
Marco Bakera <marco@bakera.de>
2018-10-25 08:21:58 +0200

    added task manager.

---
+++ src/utaskmanager.py
@@ -0,0 +1,66 @@
+'''
+Task manager for tasks that should run pseudo-parallel in micropython. 
+
+A demo application of this package is found inside the demo() method.
+'''
+import uasyncio as asyncio
+
+
+class Task:
+    TASK_SLEEP_TIME_MS = 10
+
+    def __init__(self):
+        self.task_running = False
+
+    async def start(self):
+        self.task_running = True
+        while self.task_running:
+            self.task_step()
+            await asyncio.sleep_ms(Task.TASK_SLEEP_TIME_MS)
+
+    def task_step(self):
+        '''
+        This method must be overriden by each task. It provides the method 
+        for each task step that runs in an endless loop or until 
+        self.task_running is set to false. 
+
+        Don't use time.sleep(3) in here, it will block the whole python
+        process and other tasks cannot run.
+        '''
+
+
+def add_task(task):
+    'Schedule a task for the tast manager.'
+    loop = asyncio.get_event_loop()
+    loop.create_task(task.start())
+
+def start():
+    'Start the task manager and run forever.'
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
+
+def demo():
+    # Create a class that derives from Task and implement the task_step method
+    class SampleTaskCounter(Task):
+        'Demo task that counts between two values'
+
+        def __init__(self, start_value=0, end_value=100):
+            super().__init__()  # call __init__ from Task class
+
+            # init attributes
+            self.counter = start_value
+            self.end_value = end_value
+
+        def task_step(self):
+            'Print counter value and count up if necessary'
+            print(self.counter)
+            if self.counter < self.end_value:
+                self.counter += 1
+            else:
+                self.task_running = False
+
+
+    # Add instances of the demo class to the task manager and start it.
+    add_task(SampleTaskCounter(start_value=0, end_value=10))
+    add_task(SampleTaskCounter(start_value=1000, end_value=1015))
+    start()
