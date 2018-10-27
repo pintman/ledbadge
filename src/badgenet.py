78bc6b74c4eba82ca75670f02b1683b493b557b7
Marco Bakera <marco@bakera.de>
2018-10-27 14:00:08 +0200

    added first version of server.

--- src/badgenet.py
+++ src/badgenet.py
@@ -6,8 +6,12 @@ connect to the (first) server that is found.
 import network
 import time
 
+import utaskmanager
+
 SERVER_SSID = 'ledbadge_server'
 SERVER_PASS = 'ledbadge_server'
+SERVER_IP = '192.168.4.1'
+SERVER_PORT = 1011
 WAIT_TIME_TIL_CONNECTION = 5  # seconds
 
 
@@ -29,6 +33,23 @@ def discover_server():
     else:
         return None
 
+
+class BadgeNetServer(utaskmanager.Task):
+    def __init__(self, ip, port):
+        super().__init__()
+        # connect to server
+        self.clients = []
+
+    def task_step(self):
+        # check for incoming connection
+        # if register packet: register client, sent client id to new client
+        # sent to all clients
+        pass
+
+    def register(self, ip):
+        self.clients.append(ip)
+        return len(self.clients) - 1
+
 def start_server():
     '''
     Start Server and act as access point for other badges.
@@ -37,3 +58,12 @@ def start_server():
     ap_if.config('essid', SERVER_SSID)
     ap_if.config('password', SERVER_PASS)
     ap_if.active(True)
+    bns = BadgeNetServer(SERVER_IP, SERVER_PORT)
+    utaskmanager.add_task(bns)
+
+
+
+def start():
+    ip_subnet_gw_dns = discover_server()
+    if ip_subnet_gw_dns is None:
+        start_server()
